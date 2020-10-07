13b0ef0693ab58af9d5d18e49aba7e721ab48716
Sarith <68165093+sarithdm@users.noreply.github.com>
2020-10-08 00:14:57 +0530

    Create number_system.py

---
+++ CST 283 Python for Machine Learning/Module 2/number_system.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Number system
+
+A number with the prefix 0b is considered binary, 0o is considered octal and 0x as hexadecimal
+"""
+
+dec = 10
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
+
